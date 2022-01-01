188b46e5a6ecf8387ca6eb6326c030a540af0037
Arkadiusz Kaproń <56255684+Nom0ri@users.noreply.github.com>
2022-01-01 23:42:23 +0100

    Clean up

--- src/main/java/com/nom0ri/glassmod/item/ModItems.java
+++ src/main/java/com/nom0ri/glassmod/item/ModItems.java
@@ -1,19 +1,14 @@
 package com.nom0ri.glassmod.item;
 
 import com.nom0ri.glassmod.GlassMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, GlassMod.MOD_ID);
-
-    public static final RegistryObject<Item> GLASS_X = ITEMS.register("glass_x",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
